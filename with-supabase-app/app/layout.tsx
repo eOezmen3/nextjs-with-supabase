9554f605eeec1ba60f6510db6935045f1f6db17d
Emre Oezmen <emre.oezmen@gmail.com>
2025-04-21 21:45:37 +0200

    commit2 feeature

--- with-supabase-app/app/layout.tsx
+++ with-supabase-app/app/layout.tsx
@@ -1,32 +1,33 @@
-import DeployButton from "@/components/deploy-button";
-import { EnvVarWarning } from "@/components/env-var-warning";
-import HeaderAuth from "@/components/header-auth";
-import { ThemeSwitcher } from "@/components/theme-switcher";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
-import { Geist } from "next/font/google";
-import { ThemeProvider } from "next-themes";
-import Link from "next/link";
-import "./globals.css";
+import "./globals.css"
+
+import DeployButton from "@/components/deploy-button"
+import { EnvVarWarning } from "@/components/env-var-warning"
+import { Geist } from "next/font/google"
+import HeaderAuth from "@/components/header-auth"
+import Link from "next/link"
+import { ThemeProvider } from "next-themes"
+import { ThemeSwitcher } from "@/components/theme-switcher"
+import { hasEnvVars } from "@/utils/supabase/check-env-vars"
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
+  : "http://localhost:3000"
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
   title: "Next.js and Supabase Starter Kit",
   description: "The fastest way to build apps with Next.js and Supabase",
-};
+}
 
 const geistSans = Geist({
   display: "swap",
   subsets: ["latin"],
-});
+})
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en" className={geistSans.className} suppressHydrationWarning>
@@ -63,7 +64,7 @@ export default function RootLayout({
                     className="font-bold hover:underline"
                     rel="noreferrer"
                   >
-                    Supabase
+                    Supabase with Emre Oezmen
                   </a>
                 </p>
                 <ThemeSwitcher />
@@ -73,5 +74,5 @@ export default function RootLayout({
         </ThemeProvider>
       </body>
     </html>
-  );
+  )
 }
